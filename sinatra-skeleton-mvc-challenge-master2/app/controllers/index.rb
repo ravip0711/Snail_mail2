fd35608d134154061025989fe07e99ba3362c3a1
maddisonb & Ravi <pair+maddisonb.ravip0711@devbootcamp.com>
2014-08-18 17:03:03 -0500

    status codes working

--- sinatra-skeleton-mvc-challenge-master2/app/controllers/index.rb
+++ sinatra-skeleton-mvc-challenge-master2/app/controllers/index.rb
@@ -1,5 +1,17 @@
 get '/users/:email_address/emails' do
-  if Mail.parse_mail(params[:email_address])
+  m = Mail.new(params[:email_address])
+  if m.status == "200"
+    m.messages.to_json
+  elsif m.status == "404"
+    status 404
+    "your link is bad"
+  elsif m.status == "429"
+    status 429
+  elsif m.status == "502"
+    status 502
+  else
+    "Status UNKOWN"
+  end
 end
 
 
--- sinatra-skeleton-mvc-challenge-master2/app/models/mail.rb
+++ sinatra-skeleton-mvc-challenge-master2/app/models/mail.rb
@@ -4,32 +4,37 @@ require 'net/http'
 
 class Mail
 
-  def self.parse_mail(email)
-    url = "http://dbc-mail.herokuapp.com/api/" + email + "/messages?api_token=1897625d93303cf90c957461eb1924d4"
+  def initialize(email)
+    @email = email
+    make_request
+  end
+
+  def make_request
+    url = "http://dbc-mail.herokuapp.com/api/" + @email + "/messages?api_token=1897625d93303cf90c957461eb1924d4"
     uri = URI(url)
-    http_request = Net::HTTP.get_response(uri)
-    if http_request.code == "200"
-      fetched_XML = Nokogiri::XML(open(url))
-      array_of_messages = []
-      fetched_XML.xpath("//message").each do |message|
-        array_of_messages << {
-          id: message.search("id").children.text,
-          from: message.search("from").children.text,
-          subject: message.search("subject").children.text,
-          from: message.search("from").children.text,
-          body: message.search("body").children.text
-        }
-      end
-      array_of_messages
-    elsif http_request.code = "502"
-      # status 502
-      "HELLO DUDE"
-    else
-      "HELLO"
+    @response = Net::HTTP.get_response(uri)
+  end
+
+  def status
+    @response.code
+  end
+
+  def messages
+    fetched_XML = Nokogiri::XML(@response.body)
+    array_of_messages = []
+    fetched_XML.xpath("//message").each do |message|
+      array_of_messages << {
+        id: message.search("id").children.text,
+        from: message.search("from").children.text,
+        subject: message.search("subject").children.text,
+        from: message.search("from").children.text,
+        body: message.search("body").children.text
+      }
     end
+    array_of_messages
   end
 
 end
 
-mail = Mail.parse_mail("bob@example.com")
-puts mail
+# mail = Mail.parse_mail("bob@example.com")
+# puts mail
